e368fd73de9fa7166c0aa460ece4f5869626cd7e
Eesa Patel <epatel@and.digital>
2019-06-20 15:59:22 +0100

    add click prompt to feature 1

--- src/Components/Features/1.FeatureOne/SideNav.js
+++ src/Components/Features/1.FeatureOne/SideNav.js
@@ -5,79 +5,93 @@ import { Squeeze } from 'react-burgers';
 import onClickOutside from 'react-onclickoutside';
 
 class SideNav extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            links: [
-                'Home',
-                'Path One',
-                'Path Two',
-            ],
-            activeLink: -1,
-            left: '-200%',
-            active: false 
-        };
+    this.state = {
+      links: ['Home', 'Path One', 'Path Two'],
+      activeLink: -1,
+      left: '-200%',
+      active: false
+    };
 
-        this.navOpen = this.navOpen.bind(this);
-        this.navHide = this.navHide.bind(this);
+    this.navOpen = this.navOpen.bind(this);
+    this.navHide = this.navHide.bind(this);
+  }
+
+  navOpen = () => {
+    this.setState(
+      {
+        left: '0'
+      },
+      () => {
+        document.addEventListener('click', this.navHide);
+      }
+    );
+  };
+
+  navHide = () => {
+    this.setState(
+      {
+        left: '-200%'
+      },
+      () => {
+        document.removeEventListener('click', this.navHide);
+      }
+    );
+  };
+
+  toggleBurger = () => {
+    this.setState({
+      active: !this.state.active
+    });
+  };
+
+  handleClickOutside = () => {
+    if (this.state.active === true) {
+      this.toggleBurger();
+    } else {
+      return;
     }
+  };
 
-    navOpen = () => {
-        this.setState({
-            left: '0'
-        }, () => {
-            document.addEventListener('click', this.navHide);
-        });
-    }
-
-    navHide = () => {
-        this.setState({
-            left: '-200%'
-        }, () => {
-            document.removeEventListener('click', this.navHide);
-        });
-    }
-
-    toggleBurger = () => {
-        this.setState({
-            active: !this.state.active
-        });
-    }
-
-    handleClickOutside = () => {
-        if (this.state.active === true) {
-            this.toggleBurger();
-        } else {
-            return;
-        }
-    }
-    
-    render() {
-
-        return (
-            <div className="navigationContainer">
-            <Squeeze
-                width={30}
-                lineHeight={3}
-                lineSpacing={4}
-                color='#fff'
-                className="burger" 
-                active={this.state.active} 
-                onClick={() => { this.toggleBurger(); this.navOpen(); }}
-                />
-                <nav className="sidenav" style={this.state}>
-                    <ul className="navLinkWrapper">
-                        {
-                            this.state.links.map((link, index) =>
-                                <SideNavLink key={index} index={index} link={link} onClick={this.toggleBurger} />
-                            )
-                        }
-                    </ul>
-                </nav>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='navigationContainer'>
+        <nav className='sidenav' style={this.state}>
+          <ul className='navLinkWrapper'>
+            {this.state.links.map((link, index) => (
+              <SideNavLink
+                key={index}
+                index={index}
+                link={link}
+                onClick={this.toggleBurger}
+              />
+            ))}
+          </ul>
+        </nav>
+        <div className='burgerContainer'>
+          <h3 className='changa6 white noMargin animated pulse clickHere'>
+            {this.state.active
+              ? 'Click here or anywhere to close ==>'
+              : 'Click here to open ==>'}
+          </h3>
+          <Squeeze
+            width={30}
+            lineHeight={3}
+            lineSpacing={4}
+            color='cyan'
+            className='burger'
+            active={this.state.active}
+            onClick={() => {
+              this.toggleBurger();
+              this.navOpen();
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default onClickOutside(SideNav);
--- src/Components/Features/1.FeatureOne/SideNavLink.js
+++ src/Components/Features/1.FeatureOne/SideNavLink.js
@@ -3,69 +3,79 @@ import { Link } from 'react-router-dom';
 import './SideNav.css';
 
 class SideNavLink extends Component {
-    constructor(props) {
-        super(props)
-      
-        this.state = {
-           hovered: false,
-        };
-  
-        this.mouseEnter = this.mouseEnter.bind(this);
-        this.mouseLeave = this.mouseLeave.bind(this);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hovered: false
+    };
+
+    this.mouseEnter = this.mouseEnter.bind(this);
+    this.mouseLeave = this.mouseLeave.bind(this);
+  }
+
+  mouseEnter = () => {
+    this.setState(
+      {
+        hovered: true
+      },
+      () => {
+        document.addEventListener('mouseleave', this.mouseLeave);
       }
+    );
+  };
 
-    mouseEnter = () => {
-        this.setState({
-            hovered: true
-        }, () => {
-            document.addEventListener('mouseleave', this.mouseLeave);
-        });
-    }
-    
-    mouseLeave = () => {
-        this.setState({
-            hovered: false
-        }, () => {
-            document.removeEventListener('mouseleave', this.mouseLeave)
-        });
-    }
-      
-    render() {
+  mouseLeave = () => {
+    this.setState(
+      {
+        hovered: false
+      },
+      () => {
+        document.removeEventListener('mouseleave', this.mouseLeave);
+      }
+    );
+  };
 
-        // index prop for accessing array index
-        const index = this.props.index;
-        // link prop
-        const link = this.props.link;
-        // variable for link text
-        const linkName = link;
-        // variable for home link
-        const linkHome = '/';
-        // variable for other links apart from home - remove white space
-        const linkOther = link.toLowerCase().replace(/\s+/g, '');
+  render() {
+    // index prop for accessing array index
+    const index = this.props.index;
+    // link prop
+    const link = this.props.link;
+    // variable for link text
+    const linkName = link;
+    // variable for home link
+    const linkHome = '/';
+    // variable for other links apart from home - remove white space
+    const linkOther = link.toLowerCase().replace(/\s+/g, '');
 
-        // toggle class on hover
-        const navLinkClass = this.state.hovered ? "navLink navLinkActive" : "navLink";
-        const linkInnerOneClass = this.state.hovered ? "linkInnerOne h-100 w-25 linkInnerOneActive" : "linkInnerOne h-100 w-25"
-        const linkInnerTwoClass = this.state.hovered ? "linkInnerTwo h-100 w-75 linkInnerTwoActive" : "linkInnerTwo h-100 w-75"
+    // toggle class on hover
+    const navLinkClass = this.state.hovered
+      ? 'navLink navLinkActive'
+      : 'navLink';
+    const linkInnerOneClass = this.state.hovered
+      ? 'linkInnerOne h-100 w-25 linkInnerOneActive'
+      : 'linkInnerOne h-100 w-25';
+    const linkInnerTwoClass = this.state.hovered
+      ? 'linkInnerTwo h-100 w-75 linkInnerTwoActive'
+      : 'linkInnerTwo h-100 w-75';
 
-
-        return (
-            <Link to={index === 0 ? linkHome : linkOther} 
-                className={navLinkClass} 
-                onMouseEnter={this.mouseEnter} 
-                onMouseLeave={this.mouseLeave} 
-                onClick={this.props.onClick}> 
-                <div className="linkInner d-flex h-100 w-100">
-                    <div className={linkInnerOneClass}>
-                    </div>
-                    <div className={linkInnerTwoClass}>
-                        <span className="linkText">{linkName}</span>
-                    </div>
-                </div>
-            </Link>
-        )
-    }
+    return (
+      <Link
+        to={index === 0 ? linkHome : linkOther}
+        className={navLinkClass}
+        onMouseEnter={this.mouseEnter}
+        onMouseLeave={this.mouseLeave}
+        onClick={this.props.onClick}
+      >
+        <div className='linkInner d-flex h-100 w-100'>
+          <div className={linkInnerOneClass} />
+          <div className={linkInnerTwoClass}>
+            <span className='linkText changa6'>{linkName}</span>
+          </div>
+        </div>
+      </Link>
+    );
+  }
 }
 
-export default SideNavLink
-
+export default SideNavLink;
