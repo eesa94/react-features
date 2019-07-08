02f7a97054d93e7b0f681e17a449846b8897dbcf
Eesa Patel <epatel@and.digital>
2019-07-08 09:59:35 +0100

    update feature 4 to new layout

--- src/Components/Features/4.FeatureFour/FeatureFour.js
+++ src/Components/Features/4.FeatureFour/FeatureFour.js
@@ -24,21 +24,46 @@ class FeatureFour extends Component {
   };
 
   render() {
-    const infoArray = [
-      'npm i react-parallax',
-      'Add a Parallax component and pass in the required properties e.g. bgImage, strength',
-      'Note that to see the Parallax component, you need to set two height values: the height of the Parallax component itself, and the height of an inner div',
-      'For the examples on this page, a bgImageStyle prop is passed into the Parallax component, with a height of 1300px and a width of 100%. The inner div has a set height of 100vh. This combination results in full screen hero images.',
-      'Note that this plugin is not compatible with mobile, as it uses an img element which loses proportion'
-    ];
+    const infoArray = ['react-parallax'];
 
     return (
-      <div className='container-fluid' style={{ padding: 0 }}>
-        <FeatureMenu
-          name={'React Parallax'}
-          source={'https://www.npmjs.com/package/react-parallax'}
-          info={infoArray}
-        />
+      <div className='container-fluid featureContainer' style={{ padding: 0 }}>
+        <div className='row' style={{ margin: '100px 0' }}>
+          <div className='col-6 offset-3'>
+            <FeatureMenu
+              name={'React Parallax'}
+              source={'https://www.npmjs.com/package/react-parallax'}
+              info={infoArray}
+            />
+            <div className='row'>
+              <div className='col-10 offset-1'>
+                <h1 className='white changa8 mb3'>How it Works</h1>
+                <p className='changa2 white howItWorksText'>
+                  Add a Parallax component and pass in the required properties
+                  e.g. bgImage, strength.
+                </p>
+                <p className='changa2 white howItWorksText'>
+                  Note that to see the Parallax component, you need to set two
+                  height values: the height of the Parallax component itself,
+                  and the height of an inner div.
+                </p>
+                <p className='changa2 white howItWorksText'>
+                  For the examples on this page, a bgImageStyle prop is passed
+                  into the Parallax component, with a height of 1300px and a
+                  width of 100%. The inner div has a set height of 100vh. This
+                  combination results in full screen hero images.
+                </p>
+                <p className='changa6 white howItWorksText'>
+                  Note that this plugin is not compatible with mobile, as it
+                  uses an img element which loses proportion.
+                </p>
+                <p className='changa6 white howItWorksText'>
+                  You will see 3 examples with different strengths of parallax.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <Hero
           image={benjaminvoros_edit}
