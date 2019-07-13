efeb499614e065392ae206e37caf0fb4144013fe
Eesa Patel <ep.94@hotmail.com>
2019-07-13 16:33:45 +0100

    create menu for menusectiontwo in template one; create event handlers and hover funcs

--- src/Components/SiteTemplates/1.TemplateOne/Navbar.js
+++ src/Components/SiteTemplates/1.TemplateOne/Navbar.js
@@ -3,107 +3,107 @@ import { Squeeze } from 'react-burgers';
 import NavMenu from './NavMenu';
 
 const styles = {
-  navbar: {
-    width: '100%',
-    height: '100px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    padding: '0 15%',
-    backgroundColor: 'rgba(210, 11, 33, 0)',
-    transition: '0.5s'
-  },
-  header: {
-    color: '#d20b21',
-    transition: '0.3s'
-  }
+	navbar: {
+		width: '100%',
+		height: '100px',
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		position: 'fixed',
+		top: 0,
+		left: 0,
+		padding: '0 15%',
+		backgroundColor: 'rgba(210, 11, 33, 0)',
+		transition: '0.5s'
+	},
+	header: {
+		color: '#d20b21',
+		transition: '0.3s'
+	}
 };
 
 const hoveredStyles = {
-  navbar: {
-    backgroundColor: 'rgba(210, 11, 33, 1)'
-  },
-  header: {
-    color: '#fff'
-  }
+	navbar: {
+		backgroundColor: 'rgba(210, 11, 33, 1)'
+	},
+	header: {
+		color: '#fff'
+	}
 };
 
 class Navbar extends Component {
-  state = {
-    active: false,
-    hovered: false
-  };
+	state = {
+		active: false,
+		hovered: false
+	};
 
-  toggleBurger = () => {
-    this.setState({
-      active: !this.state.active
-    });
-  };
+	toggleBurger = () => {
+		this.setState({
+			active: !this.state.active
+		});
+	};
 
-  hoveredStateTrue = () => {
-    this.setState(
-      {
-        hovered: true
-      },
-      () => {
-        document.addEventListener('mouseleave', this.hoveredStateFalse);
-      }
-    );
-  };
+	handleMouseEnter = () => {
+		this.setState(
+			{
+				hovered: true
+			},
+			() => {
+				document.addEventListener('mouseleave', this.handleMouseLeave);
+			}
+		);
+	};
 
-  hoveredStateFalse = () => {
-    this.setState(
-      {
-        hovered: false
-      },
-      () => {
-        document.removeEventListener('mouseleave', this.hoveredStateFalse);
-      }
-    );
-  };
+	handleMouseLeave = () => {
+		this.setState(
+			{
+				hovered: false
+			},
+			() => {
+				document.removeEventListener('mouseleave', this.handleMouseLeave);
+			}
+		);
+	};
 
-  render() {
-    return (
-      <div>
-        <nav
-          style={
-            this.state.hovered
-              ? { ...styles.navbar, ...hoveredStyles.navbar }
-              : styles.navbar
-          }
-          onMouseEnter={this.hoveredStateTrue}
-          onMouseLeave={this.hoveredStateFalse}
-        >
-          <h1
-            className='brand changa6'
-            style={
-              this.state.hovered
-                ? { ...styles.header, ...hoveredStyles.header }
-                : styles.header
-            }
-          >
-            BRAND
-          </h1>
-          <Squeeze
-            width={30}
-            lineHeight={3}
-            lineSpacing={4}
-            color={this.state.hovered ? '#fff' : '#d20b21'}
-            className='burger'
-            active={this.state.active}
-            onClick={() => {
-              this.toggleBurger();
-              // this.navOpen();
-            }}
-          />
-        </nav>
-        <NavMenu active={this.state.active} />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<nav
+					style={
+						this.state.hovered
+							? { ...styles.navbar, ...hoveredStyles.navbar }
+							: styles.navbar
+					}
+					onMouseEnter={this.handleMouseEnter}
+					onMouseLeave={this.handleMouseLeave}
+				>
+					<h1
+						className='brand changa6'
+						style={
+							this.state.hovered
+								? { ...styles.header, ...hoveredStyles.header }
+								: styles.header
+						}
+					>
+						BRAND
+					</h1>
+					<Squeeze
+						width={30}
+						lineHeight={3}
+						lineSpacing={4}
+						color={this.state.hovered ? '#fff' : '#d20b21'}
+						className='burger'
+						active={this.state.active}
+						onClick={() => {
+							this.toggleBurger();
+							// this.navOpen();
+						}}
+					/>
+				</nav>
+				<NavMenu active={this.state.active} />
+			</div>
+		);
+	}
 }
 
 export default Navbar;
